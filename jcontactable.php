1041edfe2c7738ed380183f112730782b5c2a6e4
Jacer OMRI <jokerhacker.jacer@gmail.com>
2013-08-06 16:19:08 +0100

    routed ajax request through wp_ajax

--- jcontactable.php
+++ jcontactable.php
@@ -6,8 +6,11 @@ Author: Jacer Omri
 Version: 1.0
 Author URI: http://jacer.info
 */
+
+//inject contactable code for public
 add_action('wp_print_scripts', 'jc_loadit');
 
+//settings for contactable
 $jc_settings = array(
 	  'jc_name' => 'Name Label'
 	, 'jc_email' => 'Email Label'
@@ -21,16 +24,8 @@ $jc_settings = array(
 	, 'jc_hideOnSubmit' => 'Hide on Submit'
 );
 
-// create custom plugin settings menu
-add_action('admin_menu', 'jc_create_menu');
-
-function jc_create_menu() {
-	add_submenu_page('options-general.php', 'jContactable Settings', 'jContactable', 'administrator', __FILE__, 'jcontactable_settings_page'/*,plugins_url('/images/icon.png', __FILE__)*/);
-	add_action( 'admin_init', 'register_jcontactable' );
-}
-
+//register the settings
 function register_jcontactable() {
-	//register our settings
 	global $jc_settings;
 	add_settings_section('jcontactable-labels', 'Labels Options', 'jcontactable_labels_code', __FILE__);
 	add_settings_section('jcontactable-settings', 'Labels Options', 'jcontactable_settings_code', __FILE__);
@@ -42,6 +37,7 @@ function register_jcontactable() {
 			add_settings_field($setting, $name, 'jcontactable_text_code',  __FILE__, 'jcontactable-labels', array($setting));
 	}
 }
+
 function jcontactable_labels_code() {
 	echo '<p>' . _e("This section allow you to configure jContactable Labels") . '</p>';
 }
@@ -59,6 +55,14 @@ function jcontactable_checkbox_code(array $args) {
 }
 
 register_activation_hook( __FILE__, 'jc_set_up_options' );
+
+// create custom plugin settings menu
+add_action('admin_menu', 'jc_create_menu');
+
+function jc_create_menu() {
+	add_submenu_page('options-general.php', 'jContactable Settings', 'jContactable', 'administrator', __FILE__, 'jcontactable_settings_page'/*,plugins_url('/images/icon.png', __FILE__)*/);
+	add_action( 'admin_init', 'register_jcontactable' );
+}
 
 function jc_set_up_options(){
 	$jc_settings_def = array(
@@ -89,6 +93,46 @@ function jcontactable_settings_page() { ?>
 	</div><?php 
 }
 
+
+//ajax functions
+add_action('wp_ajax_nopriv_send_mail', 'jc_send_mail');
+add_action('wp_ajax_send_mail', 'jc_send_mail');
+
+function jc_send_mail() {
+	$admin_email = get_bloginfo('admin_email');
+	// Assign contact info
+	$name = stripcslashes($_POST['name']);
+	$emailAddr = stripcslashes($_POST['email']);
+	$issue = stripcslashes($_POST['issue']);
+	$comment = stripcslashes($_POST['message']);
+	$subject = stripcslashes($_POST['subject']);	
+	
+	// Set headers
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+	// Format message
+	$contactMessage =  
+	"<div>
+	<p><strong>Name:</strong> $name <br />
+	<strong>E-mail:</strong> $emailAddr <br />
+	<strong>Issue:</strong> $issue </p>
+
+	<p><strong>Message:</strong> $comment </p>
+
+	<p><strong>Sending IP:</strong> $_SERVER[REMOTE_ADDR]<br />
+	<strong>Sent via:</strong> $_SERVER[HTTP_HOST]</p>
+	</div>";
+
+	// Send and check the message status
+	@$response = (mail($admin_email, $subject, $contactMessage, $headers) ) ? "success" : "failure" ;
+	$output = json_encode(array("response" => $response));
+	
+	header('content-type: application/json; charset=utf-8');
+	die($output);
+}
+
+//public output
 function jc_loadit(){
 	wp_enqueue_style( 'contactable.css', plugins_url( 'contactable.css' , __FILE__ ), array());
 	wp_enqueue_script( 'jcontactable.js', plugins_url( 'jquery.contactable.min.js' , __FILE__ ), array('jquery'));
@@ -101,7 +145,7 @@ function jc_loadit(){
 	//<![CDATA[
 	var jc = {
 		subject: 'feedback URL:'+location.href,
-		url: '<?php echo plugins_url( 'mail.php' , __FILE__ ); ?>',
+		url: ajaxurl,
 		name: '<?php echo addslashes(get_option('jc_name')); ?>',
 		email: '<?php echo addslashes(get_option('jc_email')); ?>',
 		dropdownTitle: '<?php echo addslashes(get_option('jc_dropdownTitle')); ?>',
